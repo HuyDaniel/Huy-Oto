644e8f511800f715a0fddb44c599fbc59a39e18b
Ngo Quoc Huy <145730231+HuyDaniel@users.noreply.github.com>
2026-07-22 22:52:38 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.get('/api/health', (req, res) => {
 // 🟢 PHẦN TÍCH HỢP ĐỂ RENDER CHẠY FULLSTACK (FRONTEND + BACKEND)
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
