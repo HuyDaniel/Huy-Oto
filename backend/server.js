2fa5033b4a2f480d1eed1aa469b147e6353441be
Ngo Quoc Huy <145730231+HuyDaniel@users.noreply.github.com>
2026-07-24 11:00:16 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ connectDB();
 
 // Cấu hình Middlewares
 app.use(cors());
-app.use(express.json()); // Đọc dữ liệu JSON từ body request
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Khai báo các Tuyến đường (Routes) API
 app.use('/api/auth', require('./routes/authRoutes'));
